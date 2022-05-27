ed24e0a781cfb48e696c7b8df4fb96479dd9f3d4
Kai D. Gonzalez <gkai70264@gmail.com>
2022-05-27 18:44:21 -0400

    Better format

--- Services/SongInfoProvider.gd
+++ Services/SongInfoProvider.gd
@@ -1,15 +1,14 @@
 extends Node
 
-
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
-
-export var Name = "Song Name";
-export var Author = "Unknown Artist";
+export var Name = "Song Name"
+export var Author = "Unknown Artist"
 export var SONG_LENGTH = 0
 export var Last_Saved_Time = 0
+
 
 func get_file_time(fname):
 	print(fname.get_extension())
@@ -18,7 +17,7 @@ func get_file_time(fname):
 		var f = File.new()
 		f.open(fname, File.READ)
 		var b = f.get_buffer(f.get_len())
-		
+
 		st.data = b
-		
+
 		return st.get_length()
